4428d3ca30797d5f97e6ebfa6dfc2db7e7134bb0
Joshtri <stuffofyos1516@gmail.com>
2024-07-02 11:43:39 +0800

    fix

--- routes/index.route.js
+++ routes/index.route.js
@@ -4,6 +4,10 @@ import { indexPage, createGuest } from '../controllers/index.controller.js';
 const router = express.Router();
 
 router.get('/', indexPage);
+
+router.get('/testbang', (_,res)=>{
+    res.render('test');
+})
 router.post('/post', createGuest);
 
 export default router;
